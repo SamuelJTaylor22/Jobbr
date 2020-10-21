d7ee67af9e4eaed49890fca45293908f848f60b7
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-10-21 09:54:02 -0600

    added basic frontend

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
 using Jobbr.Repositories;
 using Jobbr.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +31,29 @@ namespace Jobbr
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+              {
+                  options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                  options.Audience = Configuration["Auth0:Audience"];
+              });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsDevPolicy", builder =>
+                {
+                        builder
+                            .WithOrigins(new string[]{
+                            "http://localhost:8080",
+                            "http://localhost:8081"
+                            })
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
             services.AddControllers();
 
             services.AddScoped<IDbConnection>(x => CreateDbConnection());
@@ -50,11 +75,16 @@ namespace Jobbr
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsDevPolicy");
             }
+
+            Auth0ProviderExtension.ConfigureKeyMap(new List<string>() {"id"});
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
---
+++ wwwsrc/src/authConfig.js
@@ -0,0 +1,3 @@
+export const domain = "dev-al054o8e.us.auth0.com";
+export const clientId = "f7iRC7C4abqQLo103o3D6dbf3vhjJ87i";
+export const audience = "https://theboard";
